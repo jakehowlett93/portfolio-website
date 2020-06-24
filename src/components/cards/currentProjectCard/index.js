b9d9673cdaf868929ce87cb42b463f8d431d2dd2
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2020-06-24 09:32:01 +0100

    edited the styling a bit added some routing links

--- src/components/cards/currentProjectCard/index.js
+++ src/components/cards/currentProjectCard/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CurrentProjectCard = () => {
     return (
-        <div className="card">
-            <h3 className="card-title">React-redux course</h3>
-            <p className="card-subtitle">React Redux</p>
-            <img className="card-image" src={ require("../../../assets/current-project-thumbnail.png") } alt="current Project" />
-        </div>
+        <Link to='/current-project' className="card-link">
+            <div className="card">
+                <h3 className="card-title">Current Project</h3>
+                <p className="card-subtitle">placeholder text and image</p>
+                <img className="card-image" src={ require("../../../assets/current-project-thumbnail.png") } alt="current Project" />
+            </div>
+        </Link>
     );
 };
 
---
+++ src/components/projects/currentProject/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CurrentProject = () => {
+    return (
+        <div>Current Project</div>
+    )
+}
+
+export default CurrentProject;
