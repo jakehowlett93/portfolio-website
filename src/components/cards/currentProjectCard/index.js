8483e1e784b49b5304505034562dca3394c5498c
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2020-06-25 21:31:59 +0100

    temp commit of new styles

--- src/components/cards/currentProjectCard/index.js
+++ src/components/cards/currentProjectCard/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const CurrentProjectCard = () => {
+const CurrentProjectCard = () => { //gotta change card-title to project-title in the h3
     return (
         <Link to='/current-project' className="card-link">
             <div className="card">
-                <h3 className="card-title">Current Project</h3>
-                <p className="card-subtitle">placeholder text and image</p>
+                <div className="card-info">
+                    <h3 className="card-title">Current Project</h3>
+                    <p className="card-subtitle">placeholder text and image</p>
+                </div>
                 <img className="card-image" src={ require("../../../assets/current-project-thumbnail.png") } alt="current Project" />
             </div>
         </Link>
